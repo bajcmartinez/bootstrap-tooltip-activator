9a7d70af9c1320548bde7dd723a116a1af16a64b
Juan Cruz Martinez <juan.martinez@siemens.com>
2017-08-01 22:29:46 -0300

    Minor fixes

--- app/components/bootstrap-tooltip-activator.js
+++
@@ -1,3 +0,0 @@
-import bootstrapTooltipActivator from 'bootstrap4-tooltip-activator/components/bootstrap4-tooltip-activator';
-
-export default bootstrapTooltipActivator;
---
+++ app/components/bootstrap4-tooltip-activator.js
@@ -0,0 +1,3 @@
+import bootstrap4TooltipActivator from 'bootstrap4-tooltip-activator/components/bootstrap4-tooltip-activator';
+
+export default bootstrap4TooltipActivator;
